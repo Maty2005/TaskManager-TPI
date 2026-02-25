68c6ef8a8607a43532f46d66adceefed53445797
Maty2005 <matiaspalanca53@gmail.com>
2026-02-25 08:48:40 -0300

    Update User.cs
    Removed commented navigation properties section in User class.

--- TaskManager.Domain/Entities/User.cs
+++ TaskManager.Domain/Entities/User.cs
@@ -7,7 +7,6 @@ namespace TaskManager.Domain.Entities
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        // Navigation properties
         public virtual ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
         public virtual ICollection<TaskComment> Comments { get; set; } = new List<TaskComment>();
     }
